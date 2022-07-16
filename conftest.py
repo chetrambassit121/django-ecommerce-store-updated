1d18b4a63e2a8b394a3845dc69695c2814d49d9e
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-15 23:27:49 -0400

    implementing pytest and coverage

--- conftest.py
+++ conftest.py
@@ -5,3 +5,15 @@ import pytest
 def test_fixture1():
     print("Run once")
     return 1
+
+
+@pytest.fixture(scope="session")
+def test_fixture1():
+    print("Run once")
+    return 1
+
+
+@pytest.fixture(scope="session")
+def test_fixture1():
+    print("Run once")
+    return 1
--- tests/catalogue/test_catalogue_views.py
+++ tests/catalogue/test_catalogue_views.py
@@ -5,3 +5,16 @@ import pytest
 def test_hello_world3(test_fixture1):
     print("function_fixture3")
     assert test_fixture1 == 1
+
+
+# # use this decorator if you know test will fail
+# @pytest.mark.xfail
+# def test_hello_world3(test_fixture1):
+#     print("function_fixture3")
+#     assert test_fixture1 == 1
+
+# # use this decorator if you want to skip text
+# @pytest.mark.skip
+# def test_hello_world3(test_fixture1):
+#     print("function_fixture3")
+#     assert test_fixture1 == 1
