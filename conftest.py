59fc798031c34b306f697577be5a59a7263a57ed
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-16 13:14:02 -0400

    install pytest factories / factory_boy , in confitest created product category function, in tests/factories created class categoryfactory, in app/catalogue/tests/test_catalougue_model created test_cat_str runs successfully

--- conftest.py
+++ conftest.py
@@ -1,19 +1,33 @@
 import pytest
+from pytest_factoryboy import register
+
+from tests.factories import CategoryFactory
+
+register(CategoryFactory)
 
 
-@pytest.fixture(scope="session")
-def test_fixture1():
-    print("Run once")
-    return 1
+@pytest.fixture
+def product_category(db, category_factory):
+    category = category_factory.create()
+    return category
 
 
-@pytest.fixture(scope="session")
-def test_fixture1():
-    print("Run once")
-    return 1
+# import pytest
 
 
-@pytest.fixture(scope="session")
-def test_fixture1():
-    print("Run once")
-    return 1
+# @pytest.fixture(scope="session")
+# def test_fixture1():
+#     print("Run once")
+#     return 1
+
+
+# @pytest.fixture(scope="session")
+# def test_fixture1():
+#     print("Run once")
+#     return 1
+
+
+# @pytest.fixture(scope="session")
+# def test_fixture1():
+#     print("Run once")
+#     return 1
---
+++ tests/factories.py
@@ -0,0 +1,17 @@
+import factory
+from ecommerce.apps.catalogue.models import Category
+from faker import Faker
+
+fake = Faker()
+
+####
+# Catalogue
+####
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Category
+
+    name = "django"
+    slug = "django"
