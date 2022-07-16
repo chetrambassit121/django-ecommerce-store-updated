0d5d544ff89466254957b2fdea390ac4f42e5089
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-16 15:46:03 -0400

    created @pytest.mark.parametrize, created test_customer_add, test passed successfully

--- ecommerce/apps/account/tests/test_account_forms.py
+++ ecommerce/apps/account/tests/test_account_forms.py
@@ -1,1 +1,23 @@
 import pytest
+from ecommerce.apps.account.forms import RegistrationForm, UserAddressForm
+
+
+@pytest.mark.parametrize(
+    "full_name, phone, address_line, address_line2, town_city, postcode, validity",
+    [
+        ("mike", "02343343434", "add1", "add2", "town", "postcode", True),
+        ("", "02343343434", "add1", "add2", "town", "postcode", False),
+    ],
+)
+def test_customer_add(full_name, phone, address_line, address_line2, town_city, postcode, validity):
+    form = UserAddressForm(
+        data={
+            "full_name": full_name,
+            "phone": phone,
+            "address_line": address_line,
+            "address_line2": address_line2,
+            "town_city": town_city,
+            "postcode": postcode,
+        }
+    )
+    assert form.is_valid() is validity
