5b2991389cda7814620fd3089559ea312e4aa2b0
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-16 12:03:39 -0400

    more testing notes added in docs folder, test folder with files for catologue app

--- tests/catalogue/test_catalogue_views.py
+++ tests/catalogue/test_catalogue_views.py
@@ -1,10 +1,10 @@
-import pytest
+# import pytest
 
 
-@pytest.mark.core
-def test_hello_world3(test_fixture1):
-    print("function_fixture3")
-    assert test_fixture1 == 1
+# @pytest.mark.core
+# def test_hello_world3(test_fixture1):
+#     print("function_fixture3")
+#     assert test_fixture1 == 1
 
 
 # # use this decorator if you know test will fail
