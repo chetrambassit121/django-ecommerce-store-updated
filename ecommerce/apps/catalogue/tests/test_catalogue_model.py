73e771c4075935616b00b72401a167f841f029cd
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-16 13:31:04 -0400

    app/catalouge/tests/test_catalogue_model.py created test catagory reverse, after failing, fixed problem in category.html, then test ran successfully

--- ecommerce/apps/catalogue/tests/test_catalogue_model.py
+++ ecommerce/apps/catalogue/tests/test_catalogue_model.py
@@ -4,3 +4,10 @@ from django.urls import reverse
 
 def test_category_str(product_category):
     assert product_category.__str__() == "django"
+
+
+def test_category_reverse(client, product_category):
+    category = product_category
+    url = reverse("catalogue:category_list", args=[category])
+    response = client.get(url)
+    assert response.status_code == 200
